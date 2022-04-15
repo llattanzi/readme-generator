3ad585d24f1aecb2b6d6d4c37a6c5d8b3f13a6de
llattanzi <lauren71442@gmail.com>
2022-04-15 19:44:14 -0400

    add license badge and list generation

--- dist/README.md
+++ dist/README.md
@@ -1,8 +1,8 @@
-# fsdjkn
-  undefined
+# ghjkm
+  [![license](https://img.shields.io/badge/license-MIT-blue)](https://opensource.org/licenses/MIT) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) [![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) [![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0) 
 
   ## Description:
-  dsfkjcm
+  
 
   ## Table of Contents
   - [Description](#description)
@@ -14,20 +14,21 @@
   - [Questions](#questions)
 
   ## Installation:
-  sdfio
-
-  ## Usage:
-  wiedjao
-
-  ## Licenses:
   
 
+  ## Usage:
+  
+
+  ## Licenses:
+  MIT<br/>Apache<br/>GPLv2<br/>GPLv3<br/>
+
   ## Contribution:
-  dsfxc
+  awsd
 
   ## Tests:
-  undefined
+  
 
   ## Questions:
-  https://github.com/vgfesa 
-  For additional questions reach out to: fevaqs
+  https://github.com/ awsd
+
+  For additional questions reach out to: adwsa
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// this function also adds the link to the badge
+function renderLicenseBadge(license, licenseSection) {
+
+  // iterate through all selected licenses to build a string of badges and a bulleted
+  // list of licenses
+  for (i = 0; i < license.length; i++) {
+    if (license[i] === 'MIT') {
+      licenseSection.badges = licenseSection.badges + '[![license](https://img.shields.io/badge/license-MIT-blue)](https://opensource.org/licenses/MIT) ';
+    }
+    else if (license[i] === 'Apache') {
+      licenseSection.badges = licenseSection.badges + '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) ';
+    }
+    else if (license[i] === 'GPLv2') {
+      licenseSection.badges = licenseSection.badges + '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) ';
+    }
+    else if (license[i] === 'GPLv3') {
+      licenseSection.badges = licenseSection.badges + '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0) ';
+    }
+    else {
+      licenseSection.badges = licenseSection.badges + '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause) ';
+    }
+
+    licenseSection.list = licenseSection.list + license[i] + '<br/>';
+
+  }
+
+  return licenseSection
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// This is done in renderLicenseBadge above
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseSection = {
+    badges: '',
+    list: ''
+  };
+
+  // if there are licenses, add corresponding badges. Otherwise return empty strings
+  if (license) {
+    return renderLicenseBadge(license, licenseSection);
+  }
+  else {
+    return licenseSection;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
-  ${licenseSection}
+  ${licenseSection.badges}
 
   ## Description:
   ${data.description}
@@ -35,7 +76,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## Licenses:
-  ${data.license}
+  ${licenseSection.list}
 
   ## Contribution:
   ${data.contributors}
@@ -45,7 +86,7 @@ function generateMarkdown(data) {
 
   ## Questions:
   https://github.com/${data.github}
-  
+
   For additional questions reach out to: ${data.email}
 `;
 }
